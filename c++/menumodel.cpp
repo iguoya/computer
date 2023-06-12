af3da0629c24e521d6ad2d9d4fc1bd80e17cdce6
tiger <375478250@qq.com>
2023-06-12 23:55:44 +0800

    初步使用新方式组织菜单写法

--- c++/menumodel.cpp
+++ c++/menumodel.cpp
@@ -5,18 +5,66 @@ MenuModel::MenuModel()
     ModelColumns columns;
     model = TreeStore::create(columns);
 
-    TreeModel::Row row = *(model->append());
-    row[columns.chapter] = "John";
-    row[columns.identifier] = "中文描述";
-    row[columns.priority] = 30;
+    map<string, string>myMap{ {"STL教程","http://c.biancheng.net/stl/"},
+                              {"C语言教程","http://c.biancheng.net/c/"},
+                              {"Java教程","http://c.biancheng.net/java/"} };
 
-    Gtk::TreeModel::Row childrow = *(model->append(row.children()));
-    childrow[columns.priority] = 11;
-    childrow[columns.chapter] = "Billy Bob Junior";
+//    vector<pair<MenuNode, vector<MenuNode>>> menu
 
-    childrow = *(model->append(row.children()));
-    childrow[columns.priority] = 1222;
-    childrow[columns.chapter] = "Sue Bob";
+//    = {
+//        {
+//            "关键字用法",
+//            {
+//                {"Auto", "auto", "ssss"},
+//                {"Const", "const", "vvvvv"}
+//            }
+//        }
+//    };
+
+        vector<pair<MenuNode, vector<MenuNode>>> menu {
+            {
+                {"关键字用法", "keywords", ""},
+                {
+                    {"Auto", "auto", "ssss"},
+                    {"Const", "const", "vvvvv"}
+                }
+            },
+            {
+                {"高级语法", "grammar", ""},
+                {
+                    {"Auto", "auto", "dddd"},
+                    {"Const", "const", "1"}
+                }
+            }
+        };
+
+
+        for(auto section : menu) {
+            TreeModel::Row row = *(model->append());
+            row[columns.chapter] = section.first.chapter;
+            row[columns.identifier] = section.first.identifier;
+            row[columns.description] = section.first.description;
+            for(auto item : section.second) {
+                Gtk::TreeModel::Row child = *(model->append(row.children()));
+                child[columns.chapter] = item.chapter;
+                child[columns.identifier] = item.identifier;
+                child[columns.description] = item.description;
+            }
+        }
+
+
+    //    TreeModel::Row row = *(model->append());
+    //    row[columns.chapter] = "John";
+    //    row[columns.identifier] = "中文描述";
+    //    row[columns.priority] = 30;
+
+    //    Gtk::TreeModel::Row childrow = *(model->append(row.children()));
+    //    childrow[columns.priority] = 11;
+    //    childrow[columns.chapter] = "Billy Bob Junior";
+
+    //    childrow = *(model->append(row.children()));
+    //    childrow[columns.priority] = 1222;
+    //    childrow[columns.chapter] = "Sue Bob";
 
 
     //    menuModel = TreeStore::create(m_menuColumns);
--- c++/menumodel.h
+++ c++/menumodel.h
@@ -2,9 +2,23 @@
 #define MENUMODEL_H
 
 #include <gtkmm.h>
+#include <vector>
+//#include <string>
 
+using namespace std;
 using namespace Glib;
 using namespace Gtk;
+
+
+class MenuNode {
+public:
+    MenuNode(ustring c, ustring i, ustring d):
+        chapter(c), identifier(i), description(d){};
+    ustring chapter;
+    ustring identifier;
+    ustring description;
+//        uint8_t priority;
+};
 
 class MenuModel
 {
@@ -19,15 +33,19 @@ public:
         ModelColumns() {
             add(chapter);
             add(identifier);
-            add(priority);
+            add(description);
+//            add(priority);
         }
 
         TreeModelColumn<ustring> chapter;
         TreeModelColumn<ustring> identifier;
-        TreeModelColumn<uint8_t> priority;
+        TreeModelColumn<ustring> description;
+//        TreeModelColumn<uint8_t> priority;
     };
 private:
     RefPtr<TreeStore> model;
+
+
 };
 
 #endif // MENU_H
